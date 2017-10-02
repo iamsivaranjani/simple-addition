a58e7a53ea2b76f74cfbbaad59bb53fb0093dcf7
iamsivaranjani <sivuranjani007@example.com>
2017-10-02 22:59:57 +0530

    starting command

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b,c;
+   clrscr();
+   printf("\n enter the a,b");
+   scanf("\n%d %d" &a,&b);
+      c=a+b;
+   printf("\n %d" c);
+   return(0);
+}
